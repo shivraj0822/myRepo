6deaf0497ae487ed1a96fcb4936c63788321eea8
shivraj0822 <daman.gautam0822@gmail.com>
2024-04-11 20:48:17 +0545

    Create class animal

--- day8/NETStarter/Basics/Program.cs
+++ day8/NETStarter/Basics/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Animal Tiger = new();
+Animal Cow = new();
+
+
+Tiger.name = "Tiger";
+Tiger.habitat = "Wild";
+Tiger.foodtype = "Carnivorous";
+Tiger.lifeduration = 10;
+Tiger.leg = 4;
+
+Tiger.Animaldetails();
+
+
+Cow.name = "Cow";
+Cow.habitat = "Domestic";
+Cow.foodtype = "harnivorous";
+Cow.lifeduration = 15;
+Cow.leg = 4;
+
+Cow.Animaldetails();
--- day8/NETStarter/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
+++ day8/NETStarter/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c410e161850814f72a72fc385b8620bfa395c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a169abc9f5c73fb04f296480848ef4adf3e0fac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
